2a9c3e566fb2ead4e13917b7eadbc02bf7140dfc
VittoriaVecchioli <vittoria.vecchioli@dauphine.eu>
2021-04-14 18:11:02 +0200

    first commit product

--- php/products.php
+++ php/products.php
@@ -6,13 +6,104 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Products</title>
+		<link rel="stylesheet" href="../css/style.css" />
+
+		<body>
+
+			<div id='prod_banner'>
+			</div>
+
+				<div id="prod_wrap">
+				<div id="prod_columns" class="prod_col">
+				
+				<figure>
+				<img src="../images/fables.jpg" >
+				<figcaption>Fables <br>La Fontaine</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Descriptions</a>
+				</figure>
+				
+				<figure>
+				<img src="../images/lesmiserables.jpg">
+				<figcaption>Les Miserables<br>Victor Hugo</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+
+				<figure>
+				<img src="../images/lecid.png">
+				<figcaption>Le Cid<br>Pierre Corneille</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+				
+				<figure>
+				<img src="../images/memoires.jpg">
+				<figcaption>Memoires d'Hadrien<br>Marguerite Yourcenar</figcaption>
+				<span class="price">$7.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+
+				<figure>
+				<img src="">
+				<figcaption>Les Miserables</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+				
+				<figure>
+				<img src="">
+				<figcaption>Les Miserables</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+
+				<figure>
+				<img src="">
+				<figcaption>Les Miserables</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+				
+				<figure>
+				<img src="">
+				<figcaption>Les Miserables</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+
+				<figure>
+				<img src="">
+				<figcaption>Les Miserables</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+				
+				<figure>
+				<img src="">
+				<figcaption>Les Miserables</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+
+				<figure>
+				<img src="">
+				<figcaption>Les Miserables</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+				
+				<figure>
+				<img src="">
+				<figcaption>Les Miserables</figcaption>
+				<span class="price">$8.50</span>
+				<a class="prod_button" href="#">Description</a>
+				</figure>
+
+			</div>
+
+</body>
 	</head>
-	<body>
-		<div class="Product">
-			Product 1
-		</div>
-		<div class="Product">
-			Product 2
-		</div>
-	</body>
+
 </html>
+
