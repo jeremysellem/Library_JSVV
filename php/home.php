85bde78821bf9121cbc54642d995f98d2053dba8
jeremysellem <jeremsellem@gmail.com>
2021-04-14 12:19:57 +0200

    Done main page + footer

--- php/home.php
+++ php/home.php
@@ -26,7 +26,8 @@
 		<link rel="stylesheet" href="../css/style.css" />
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
-		<script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript">
 			function resizeIframe(obj) {
 				obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
 			}
@@ -35,34 +36,34 @@
 	<body>
 		<header>
 			<nav>
-				<ul>
-					<li>
-				        <a href="../html/main.html" target="Main" class="logo">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+				        <a href="home.php" target="_self" class="logo" style="text-decoration: none;">
 				        	<img src="../images/website_logo.png" width="60" height="60">
-				        	<h1>J&V Library</h1>
+				        	<h1 style="color: #1E1E1E;">J&V Library</h1>
 				        </a>
 			    	</li>
-			    	<li>
+			    	<li class="nav-item active" id="home">
 						<a href="../html/main.html" target="Main">
 							<h1>Home</h1>
 						</a>
 					</li>
-			    	<li>
+			    	<li class="nav-item">
 						<a href="products.php" target="Main">
 							<h1>Products</h1>
 						</a>
 					</li>
-					<li>
+					<li class="nav-item">
 						<a href="books.php" target="Main">
 							<h1>Books</h1>
 						</a>
 					</li>
-					<li>
+					<li class="nav-item">
 						<a href="buy.php" target="Main">
 							<h1>Buy</h1>
 						</a>
 					</li>
-					<li>
+					<li class="nav-item">
 						<a href="update.php" target="Main">
 							<h1>Update</h1>
 						</a>
@@ -82,10 +83,17 @@
 			<a href="legal.php" target="Main">
 				<h2>Legal</h2>
 			</a>
-	        <a href="../html/main.html" target="Main" class="logo">
-	        	<img src="../images/website_logo.png" width="60" height="60">
-	        	<h2>J&V Library</h2>
+	        <a href="../html/main.html" target="Main" style="text-decoration: none;">
+	        	<h2>J&V Library &#169;</h2>
 	        </a>
 	    </footer>
+	    <script type="text/javascript">
+	    	$(document).ready(function () {
+				$('ul.navbar-nav > li').click(function (e) {
+					$('.navbar-nav li').removeClass('active');
+					$(this).addClass('active');
+				});
+			});
+	    </script>
 	</body>
 </html>
